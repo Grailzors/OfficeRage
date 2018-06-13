9757295bfa55df306dae3bd02d350973527327c5
Matt Payne <matthew.payne1988@gmail.com>
2018-06-14 10:50:35 +1200

    Boss Scripts
    BossMovement.cs
    BossManager.cs

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour {
+
+    public float moveSpeed = 2f;
+
+    //Get the path system set up an then sort this stuff out
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
