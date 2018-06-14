d7b49877b2f6b4b53c76b167df3b06a32c69ee4b
Matt Payne <matthew.payne1988@gmail.com>
2018-06-15 08:12:26 +1200

    BossMovement.cs update

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -6,12 +6,34 @@ public class BossMovement : MonoBehaviour {
 
     public float moveSpeed = 2f;
 
-    //Get the path system set up an then sort this stuff out
+    private GameObject player;
 
+    //Get the path system set up an then sort this stuff out
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void Update()
     {
-        
+        FollowPlayer();
     }
 
+    void FollowPlayer()
+    {
+        //This moves the boss to the player (chasing the player)
+        //Current issue where the boss pushes the player needs to be fixed in the collider
+        float step = moveSpeed * Time.deltaTime;
+        Vector3 playerPos = player.transform.position;
+
+        if (GameManager.bossDetection && PlayerManager.isWorking == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerPos, step);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0f, 0f, 0f), step);
+            GameManager.bossDetection = false;
+        }
+    }
 }
