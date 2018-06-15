19724adec404f4bebc529a0deaecdafd52f25f8b
Matt Payne <matthew.payne1988@gmail.com>
2018-06-15 15:38:10 +1200

    Path assets and scripts

--- Assets/Scripts/Path/PathManager.cs
+++ Assets/Scripts/Path/PathManager.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class PathManager : MonoBehaviour {
 
-    public Transform[] nodes;
+    public Color nodeColor = Color.cyan;
+    public Color lineColor = Color.white;
+    public float nodeRadius = 1f;
+    public List<Transform> nodes; 
+    
+    private void OnDrawGizmosSelected()
+    {
+        nodes = new List<Transform>();
+
+        //Puts the children of the PathManager into a list for node
+        //visuallization
+        for (int child = 0; child < transform.childCount; child++)
+        {
+            nodes.Add(transform.GetChild(child));
+        }
+
+        //Creates the visuals for the nodes
+        for (int i = 0; i <= nodes.Count; i++)
+        {
+            Transform target = nodes[i+1];
+
+            Gizmos.color = nodeColor;
+            Gizmos.DrawSphere(nodes[i].position, nodeRadius);
+
+            //Draw lines between the nodes to show the path
+            if (target != null)
+            {
+                Gizmos.color = lineColor;
+                Gizmos.DrawLine(nodes[i].position, target.position);
+            }
+        }
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
